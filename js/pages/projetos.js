f16c6260208688ff6ed8501d5bcd726089692fa9
Isaac Hermel <isaachermel@gmail.com>
2021-11-29 11:41:31 -0300

    projects new feature (image)

--- js/pages/projetos.js
+++ js/pages/projetos.js
@@ -9,26 +9,30 @@ const projetos = `
         em <strong><a href="https://github.com/IsaacHermel" target="_blank" rel="noopener noreferrer">meu GitHub</a></strong>:
         </p>
         <ul class="projetos--list">
-        <li class="projeto" id="bootstrap">
-            <span class="projetos--type">Front-End</span>
-            <h3 class="projetos--title">Site responsivo em Bootstrap 4</h3>
-            <a
-            href="https://isaachermel.github.io/Bootstrap_course/"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="projetos--link"
-            >Link para acesso</a
-            >
-            <ul class="habilities--list">
-            <li class="hability--html">HTML5</li>
-            <li class="hability--css">CSS3</li>
-            <li class="hability--js">JavaScript</li>
-            <li class="hability--bootstrap">Bootstrap 4</li>
-            </ul>
-        </li>
+            <li class="projeto" id="bootstrap">
+                <div class="projetos--info">
+                    <span class="projetos--type">Front-End</span>
+                    <h3 class="projetos--title">Site responsivo em Bootstrap 4</h3>
+                    <a
+                    href="https://isaachermel.github.io/Bootstrap_course/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="projetos--link"
+                    >Link para acesso</a
+                    >
+                    <ul class="habilities--list">
+                        <li class="hability--html">HTML5</li>
+                        <li class="hability--css">CSS3</li>
+                        <li class="hability--js">JavaScript</li>
+                        <li class="hability--bootstrap">Bootstrap 4</li>
+                    </ul>
+                </div>
+                <div class="projetos--image">
+                    <img src="../img/bootstrap.png" alt="Bootstrap 4 Website"/>
+                </div>
+            </li>
         </ul>
     </div>
 </div>
 `;
-
 export default projetos;
